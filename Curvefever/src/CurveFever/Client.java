105c7fd668105b2e7878e74f19ba186d383865be
Frici <fbalazs99@gmail.com>
2022-05-10 16:39:10 +0200

    First sketch of Client and Server.
    This commit is only a backup

--- Curvefever/src/CurveFever/Client.java
+++ Curvefever/src/CurveFever/Client.java
@@ -1,12 +1,23 @@
 package CurveFever;
 
-public class Client {
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client{
     private Player player;
     private Board board;
     private boolean isGameHost;
     private String serverIp;
 
-    //For client
+    private Socket socket;
+    private ObjectInputStream objIStream;
+    private ObjectOutputStream objOStream;
+
+    protected static final int port = 7785;
+
+
     public Client(String serverIp, String playerName, boolean isGameHost) {
         if (isGameHost) {
             this.serverIp = "localhost";
@@ -56,13 +67,80 @@ public class Client {
 
     public void sendToServer() {
 
+        /* if the connection got closed, or it has never been started */
+        if (socket == null || socket.isClosed() ) {
+            establishConnection();
+        }
+
+        try {
+            objOStream.writeObject(player.getControlState());
+            objOStream.flush();
+        } catch (IOException e) {
+            System.out.println("IOexception while trying to send");
+        }
+
     }
 
     public PackageS2C receiveFromServer() {
+        /* if the connection got closed, or it has never been started */
+        if (socket == null || socket.isClosed() ) {
+            establishConnection();
+        }
+
+        try {
+            PackageS2C message  = (PackageS2C) objIStream.readObject();
+            return message;
+        } catch (IOException e) {
+            System.out.println("IOexception while trying to receive");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException");
+        }
+
+
+        /* if we get to here that's an error */
         return null;
     }
 
     public InitPackageS2C receiveFromServerInit() {
+        /* if the connection got closed, or it has never been started */
+        if (socket == null || socket.isClosed() ) {
+            establishConnection();
+        }
+
+        try {
+            InitPackageS2C message  = (InitPackageS2C) objIStream.readObject();
+            return message;
+        } catch (IOException e) {
+            System.out.println("IOexception while trying to receive");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException");
+        }
+
+
+        /* if we get to here that's an error */
         return null;
+    }
+
+
+    private void establishConnection() {
+
+        //TODO port megadast kitalalni? külön megadni, kiszedni a stringnbol vagy hardcode?
+
+        System.out.println("Connecting to server...");
+        try {
+            socket = new Socket(InetAddress.getByName(serverIp),port);
+            InputStream iStream = socket.getInputStream();
+            OutputStream oStream = socket.getOutputStream();
+
+            objIStream = new ObjectInputStream(iStream);
+            objOStream = new ObjectOutputStream(oStream);
+
+        } catch (UnknownHostException e) {
+            System.out.println("Server IP not valid");
+        } catch (IOException e) {
+            System.out.println("IOException when trying to connect");
+        }
+
+        System.out.println("Connected to server successful");
     }
 }
--- Curvefever/src/CurveFever/Server.java
+++ Curvefever/src/CurveFever/Server.java
@@ -1,15 +1,37 @@
 package CurveFever;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server extends Client{
 
     private Game game;
-    private String[] ClientIpAddresses;
-    private int port;
+    private int numOfClients;   //NOT the number of players!!!!!!!!
+    private ServerSocket ss;
+    private Socket[] Sockets;
+    private ObjectOutputStream[] ObjOutStreams;
+    private ObjectInputStream[]  ObjInStreams;
+    private GetControl[] GCRunnables;
+    private SendPoints[] SPRunnables;
 
-    public Server(int port, String playerName) {
+
+    //private int port;
+
+    public Server(int numOfPlayers, String playerName) {
         super("", playerName, true);
 
-        this.port = port;
+        numOfClients = numOfPlayers - 1;
+        //this.port = port;
+
+        try {
+            ss = new ServerSocket(port);
+        } catch (IOException ex) {
+            System.out.println("IOException from server constructor");
+        }
+        System.out.println("----- Server created -----");
     }
 
     public void setClientIpAddresses(String[] addresses) {
@@ -21,9 +43,9 @@ public class Server extends Client{
         this.game = game;
     }
 
-    public void setPort (int port) {
+    /*public void setPort (int port) {
         this.port = port;
-    }
+    }*/
 
     public Game getGame() {
         return game;
@@ -42,8 +64,51 @@ public class Server extends Client{
         return port;
     }
 
+    public void acceptConnections()
+    {
+        int numOfPlayer = 0;
+
+        System.out.println("Waiting for connections.... (1/"+(numOfClients+1)+")";
+        try {
+            while (numOfPlayer < numOfClients) {
+                /* accept new connection */
+                Sockets[numOfPlayer] = ss.accept();
+
+                /* store IO streams, and create runnables */
+                ObjOutStreams[numOfPlayer] = new ObjectOutputStream(Sockets[numOfPlayer].getOutputStream());
+                ObjInStreams[numOfPlayer] = new ObjectInputStream(Sockets[numOfPlayer].getInputStream());
+
+                SPRunnables[numOfPlayer] = new SendPoints(numOfPlayer);
+                GCRunnables[numOfPlayer] = new GetControl(numOfPlayer);
+
+                /* increment player number */
+                numOfPlayer++;
+                System.out.println("New player connected. "  + numOfPlayer+ "/" + (numOfClients+1)) ;
+            }
+            System.out.println("All " + numOfClients + 1 + " players connected");
+        } catch (IOException ex) {
+            System.out.println("IOException from acceptConnections()");
+        }
+
+    }
+
+    public void setupGame(){
+        //TODO blokkolva elküldi a dolgokat.
+    }
+
 
     public void sendToClient(boolean InitPackage) {
+        /* if the connection got closed, or it has never been started */
+        if (socket == null || socket.isClosed() ) {
+            establishConnection();
+        }
+
+        try {
+            objOStream.writeObject(player.getControlState());
+            objOStream.flush();
+        } catch (IOException e) {
+            System.out.println("IOexception while trying to send");
+        }
 
     }
 
@@ -53,5 +118,57 @@ public class Server extends Client{
 
     public void run() {
 
+    }
+
+    /* private halper classes */
+    private class GetControl implements Runnable {
+
+        private final int clientId;
+
+
+
+        public GetControl(int clientId){
+
+            this.clientId = clientId;
+            System.out.println("GetControl runnable for client #" + clientId + "created");
+        }
+        public void run() {
+
+            try {
+
+                /* send out a request */
+                ObjOutStreams[clientId].writeUTF("Provide control input pls");
+                ObjOutStreams[clientId].flush();
+
+                //TODO megcsinalni hogy a thread a tenyleges valtozot allitsa
+                ControlState dummy;
+                dummy = (ControlState)ObjInStreams[clientId].readObject();
+            } catch (IOException e) {
+                System.out.println("IOException from getControl runnable #" + clientId);
+            } catch (ClassNotFoundException e) {
+                System.out.println("ClassNotFoundException from getControl runnable #" + clientId);
+            }
+        }
+    }
+
+    private class SendPoints implements Runnable {
+
+        private final int clientId;
+
+        public SendPoints(int clientId) {
+            this.clientId = clientId;
+            System.out.println("SendPoints runnable for client #" + clientId + "created");
+        }
+
+        public void run() {
+
+            try {
+                //TODO megcsinalni hogy a tenyleges adatot kuldje
+                PackageS2C dummyForClient = new PackageS2C(4);
+                ObjOutStreams[clientId].writeObject(dummyForClient);
+            } catch (IOException e) {
+                System.out.println("IOException from SendPoints runnable #" + clientId);
+            }
+        }
     }
 }
