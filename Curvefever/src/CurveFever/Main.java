3df30df405e11f404f88d695124565ba54b81a60
Frici <fbalazs99@gmail.com>
2022-05-24 23:20:29 +0200

    Added sketch for main, and Menu class

--- Curvefever/src/CurveFever/Main.java
+++ Curvefever/src/CurveFever/Main.java
@@ -1,10 +1,16 @@
 package CurveFever;
 
-import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.runMenu();
+    }
+
+
 
     /* this is a file to test baoard and menu objects */
     /* created by Dani */
@@ -31,37 +37,5 @@ public class Main {
         return curve;
     }
 
-    public static void main(String[] args) {
-        //generating some random curves
-        Curve curve = generateRandomCurve(500,0, 720, PINK, 70);
-        Curve curve1 = generateRandomCurve(500,0, 720, BLUE, 70);
-        Curve curve2 = generateRandomCurve(500,0, 720, RED, 70);
-        Curve curve3 = generateRandomCurve(500,0, 720, GREEN, 70);
 
-
-        JFrame window = new JFrame("Kurve Fívör");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // when we close the window, stop the app
-        Board board = new Board(4,5,new String[]{"Hanti","Dani", "Frici","valaki"},new Color[]{PINK,PINK,PINK,PINK});
-        board.addInfoPanel();
-        //window.setSize(new Dimension(2000,2000));
-        //window.add(board); // add the jpanel to the window
-        /*JPanel probapanel = new JPanel();
-        probapanel.setBackground(Color.red);
-        probapanel.setPreferredSize(new Dimension(100,720));
-        probapanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        probapanel.setAlignmentY(Component.CENTER_ALIGNMENT);
-        window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.X_AXIS));*/
-        window.add(board);
-        //window.add(probapanel);
-
-        window.setResizable(false); // don't allow the user to resize the window
-        ImageIcon logo = new ImageIcon("src/curvefeverlogo.jpg");
-        window.setIconImage(logo.getImage()); //adding logo to window
-        window.pack(); // fit the window size around the components (just our jpanel).
-
-        window.setLocationRelativeTo(null); // open window in the center of the screen
-        board.setCurves(new Curve[]{curve, curve1, curve2, curve3}); //setting the random curves
-        //board.render(); //repainting the board //commented out to test the gui
-        window.setVisible(true); // display the window
-    }
 }
--- Curvefever/src/CurveFever/Menu.java
+++ Curvefever/src/CurveFever/Menu.java
@@ -1,35 +1,40 @@
 package CurveFever;
 
-import javax.swing.*;
+import CurveFever.gui.GameScreen;
+import CurveFever.gui.ScreenManager;
 
-public class Menu extends  JPanel{
+import javax.swing.*;
+import java.awt.*;
+
+public class Menu {
     private Client client;
     private Server server; //only one Server or one Client will be used
-    private ProgramState programState;
+    //private ProgramState programState;
     private ControlOptions controlOptions;
     private String serverIp; //To give to Server or Client constructor
     private String playerName; //To give to Player constructor (player name can be entered in menu, in a textbox)
-    private int numOfPlayers;
-    private int numOfRounds;
+    //private int numOfPlayers;
+    //private int numOfRounds;
+    //TODO ha nagyjabol kesz a menu osztaly akkor ezt purgalni
+
+    private ScreenManager sManager;
 
     public Menu(){}
 
-    public void setProgramState(ProgramState programState) {this.programState = programState;}
+    //public void setProgramState(ProgramState programState) {this.programState = programState;}
     public void setControlOptions(ControlOptions controlOptions) {this.controlOptions = controlOptions;}
     public void setServerIp(String serverIp) {this.serverIp = serverIp;}
     public void setPlayerName(String playerName) {this.playerName = playerName;}
-    public void setNumOfPlayers(int numOfPlayers) {this.numOfPlayers = numOfPlayers;}
-    public void setNumOfRounds(int numOfRounds) {this.numOfRounds = numOfRounds;}
+    //public void setNumOfPlayers(int numOfPlayers) {this.numOfPlayers = numOfPlayers;}
+    //public void setNumOfRounds(int numOfRounds) {this.numOfRounds = numOfRounds;}
     public Client getClient() {return client;}
     public Server getServer() {return server;}
-    public ProgramState getProgramStateState() {return programState;}
+    //public ProgramState getProgramStateState() {return programState;}
     public ControlOptions getControlOptions() {return controlOptions;}
     public String getServerIp() {return serverIp;}
     public String getPlayerName() {return playerName;}
-    public int getNumOfPlayers() {return numOfPlayers;}
-    public int getNumOfRounds() {return numOfRounds;}
-
-
+    //public int getNumOfPlayers() {return numOfPlayers;}
+    //public int getNumOfRounds() {return numOfRounds;}
 
 
     public void createGame(){
@@ -46,5 +51,83 @@ public class Menu extends  JPanel{
 
     public void renderMenu(){
         //Drawing Menu
+    }
+
+    public void runMenu(){
+        JFrame window = new JFrame("Kurve Fívör gui");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ScreenManager screenManager = new ScreenManager(4);
+
+        window.add(screenManager);
+        window.pack();
+        window.setVisible(true);
+
+        /* wait for the player to start click create game or joing game */
+        while (screenManager.getProgramState() != ProgramState.MAIN_MENU) {
+            screenManager.update();
+        }
+
+        /* if this instance is a server, create the server, and start the game */
+        if (screenManager.getIsServer == true){
+            /* create server */
+            int numOfPlayers = screenManager.getNumOfPlayers();
+            int numOfRounds = screenManager.getNumOfRounds();
+            String serverPlayerName = screenManager.getPlayerName();
+            this.server = new Server(numOfPlayers,numOfRounds,serverPlayerName);
+            this.client = null;
+
+            /* setup connections and configure game */
+            this.server.acceptConnections();
+            this.server.setupGame();
+
+            /* eneter game loop */
+            while (screenManager.getProgramState() == ProgramState.IN_GAME) {
+                Board boardToDisplay = this.server.getGame().getMainBoard(); //todo ezt optimalizalni
+                screenManager.getGameScreen().setCurves(boardToDisplay.getCurves());
+                screenManager.getGameScreen().setCurrentRound(boardToDisplay.getCurrentRound());
+                screenManager.getGameScreen().setPlayerNames(boardToDisplay.getPlayerNames());
+                screenManager.getGameScreen().setNumOfPlayers(boardToDisplay.getRoundNum());
+                screenManager.getGameScreen().setPoints(boardToDisplay.getPoints());
+
+                screenManager.update();
+            }
+        }
+
+        /* if this insatance is a client, setup the client, and start the game */
+        else {
+            /* create client */
+            String playerName = screenManager.getPlayerName();
+            String serverIp = screenManager.getServerIp();
+            client = new Client(serverIp,playerName,false); //todo utolso argra nincs szukseg
+            server = null;
+            /* wait for server to send init package,
+             * containing other the other players' data
+             */
+            client.receiveFromServerInit();
+
+            //TODO itt meg kene oldani hogy a player egyhelyben forogjon
+
+            /* enter game loop */
+            while (screenManager.getProgramState() == ProgramState.IN_GAME) {
+
+                /* wait for server to request data, and then send it */
+                client.sendToServer();
+
+                /* receive game data from the latest cycle */
+                PackageS2C message = myClient.receiveFromServer();
+                if (message != null )  {
+                    this.client.board.receiveFromPackageS2C(message);
+                }
+
+                Board boardToDisplay = this.client.getBoard();
+                screenManager.getGameScreen().setCurves(boardToDisplay.getCurves());  //todo ezt is optimalizalni
+                screenManager.getGameScreen().setCurrentRound(boardToDisplay.getCurrentRound());
+                screenManager.getGameScreen().setPlayerNames(boardToDisplay.getPlayerNames());
+                screenManager.getGameScreen().setNumOfPlayers(boardToDisplay.getRoundNum());
+                screenManager.getGameScreen().setPoints(boardToDisplay.getPoints());
+
+                screenManager.update();
+            }
+        }
     }
 }
